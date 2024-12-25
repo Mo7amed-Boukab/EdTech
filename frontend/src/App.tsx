2a733c4ae49b39de1711749a332c3d13e8d58099
mohamed <mohamedboukab202@gmail.com>
2024-12-25 20:00:00 +0100

    feat: initialize frontend with React, TypeScript and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-blue-300">EdTech</h1>
+        <p className="text-xl text-gray-900">EdTech is an attendance management System</p>
+      </div>
+    </>
+  )
+}
+
+export default App
